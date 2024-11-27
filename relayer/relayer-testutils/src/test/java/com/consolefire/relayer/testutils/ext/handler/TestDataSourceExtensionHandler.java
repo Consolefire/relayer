7d350a7f0f42ce74e6f93b9bb100ea6a8b6e03c7
sabuj-das <sabuj.das@gmail.com>
2024-11-27 23:48:19 +0100

    formatted

--- relayer/relayer-testutils/src/test/java/com/consolefire/relayer/testutils/ext/handler/TestDataSourceExtensionHandler.java
+++ relayer/relayer-testutils/src/test/java/com/consolefire/relayer/testutils/ext/handler/TestDataSourceExtensionHandler.java
@@ -1,23 +1,28 @@
 package com.consolefire.relayer.testutils.ext.handler;
 
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.platform.commons.support.AnnotationSupport.findAnnotatedFields;
+
 import com.consolefire.relayer.testutils.data.H2InMemoryDataSourceBuilder;
 import com.consolefire.relayer.testutils.data.TestDataSource;
 import com.consolefire.relayer.testutils.data.TestDataSourceBuilder;
-import org.junit.jupiter.api.extension.*;
-import org.junit.platform.commons.support.ModifierSupport;
-
-import javax.sql.DataSource;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.platform.commons.support.AnnotationSupport.findAnnotatedFields;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.platform.commons.support.ModifierSupport;
 
 public class TestDataSourceExtensionHandler
-        implements BeforeAllCallback, ParameterResolver {
+    implements BeforeAllCallback, ParameterResolver {
 
     public static final String EXT_CTXT_KEY_DATA_SOURCE = "test-data-source-context-key";
 
@@ -31,33 +36,34 @@ public class TestDataSourceExtensionHandler
 
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
-            throws ParameterResolutionException {
+        throws ParameterResolutionException {
         return parameterContext.isAnnotated(TestDataSource.class)
-                && isDataSource(parameterContext.getParameter().getType());
+            && isDataSource(parameterContext.getParameter().getType());
     }
 
     @Override
     public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
-            throws ParameterResolutionException {
+        throws ParameterResolutionException {
         return getOrBuildDataSource(extensionContext);
     }
 
     private DataSource getOrBuildDataSource(ExtensionContext extensionContext) {
         ExtensionContext rootContext = extensionContext.getRoot();
         ExtensionContext.Store store = rootContext.getStore(ExtensionContext.Namespace.GLOBAL);
-        return store.getOrComputeIfAbsent(EXT_CTXT_KEY_DATA_SOURCE, __ -> dataSourceBuilder.buildDataSource(), DataSource.class);
+        return store.getOrComputeIfAbsent(EXT_CTXT_KEY_DATA_SOURCE, __ -> dataSourceBuilder.buildDataSource(),
+            DataSource.class);
     }
 
     private void injectDataSource(ExtensionContext extensionContext,
-                                  Predicate<Field> predicate) {
+        Predicate<Field> predicate) {
         predicate = predicate.and(field -> DataSource.class.getTypeName().equals(field.getType().getTypeName()));
 
         Class<?> testClass = extensionContext.getRequiredTestClass();
         assertNotNull(testClass);
         Optional<Object> testInstance = extensionContext.getTestInstance();
         assertAll(
-                () -> assertNotNull(testInstance),
-                () -> assertTrue(testInstance.isPresent())
+            () -> assertNotNull(testInstance),
+            () -> assertTrue(testInstance.isPresent())
         );
 
         List<Field> annotatedFields = findAnnotatedFields(testClass, TestDataSource.class, predicate);
