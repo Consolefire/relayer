a28b44872466fd23d53dd84f4f145e57acd591f7
sabuj-das <sabuj.das@gmail.com>
2025-02-24 09:04:17 +0100

    adding parked reader

---
+++ relayer-core/src/main/java/com/consolefire/relayer/core/data/ParkedGroupReadQueryProvider.java
@@ -0,0 +1,7 @@
+package com.consolefire.relayer.core.data;
+
+public interface ParkedGroupReadQueryProvider {
+
+    String getReadQueryForAllGroupIdentifiers();
+
+}
--- relayer-core/src/main/java/com/consolefire/relayer/core/reader/ParkedGroupReader.java
+++ relayer-core/src/main/java/com/consolefire/relayer/core/reader/ParkedGroupReader.java
@@ -1,7 +1,11 @@
 package com.consolefire.relayer.core.reader;
 
 import com.consolefire.relayer.model.ParkedGroup;
+import com.consolefire.relayer.model.source.MessageSourceProperties;
+import java.util.Set;
 
 public interface ParkedGroupReader<G extends ParkedGroup> {
+
+    Set<String> getAllGroupIdentifiers(MessageSourceProperties messageSourceProperties);
 
 }
